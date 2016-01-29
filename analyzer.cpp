7d620f7a2d3cdfa61428ded31edb80055caf7f29
Yu hojei <hojei1452@naver.com>
2016-01-29 12:02:18 -0800

    analyzer

--- analyzer.cpp
+++ analyzer.cpp
@@ -7,5 +7,15 @@ Analyzer::Analyzer(QObject *parent) : QObject(parent)
 
 void Analyzer::doStart()
 {
+    while(threadStatu)
+    {
+        pcap_pkthdr* header;
+        const u_char* pkt_data;
 
+        int res = pcap_next_ex(handle, &header, &pkt_data);
+        if(res == 0)
+            continue;
+        else if(res < 0)
+            break;
+    }
 }
